768b765fff4b68f609a9f9fdd9bc60ce9e2e5e9e
PeterShipp <pshipp81@aol.com>
2020-03-29 13:55:53 -0500

    Add project files.

---
+++ Lesson12_SortCompare/SuperHeroComparers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson12_SortCompare
+{
+    class SuperSpeedComparer : Comparer<SuperHero>
+    {
+
+        public override int Compare(SuperHero x, SuperHero y)
+        {
+            if (x.speed > y.speed)
+                return -1;
+            else if (x.speed < y.speed)
+                return 1;
+            else
+                return 0;
+
+
+        }
+    }
+
+    class SuperStrengthComparer : Comparer<SuperHero>
+    {
+
+        public override int Compare(SuperHero x, SuperHero y)
+        {
+            if (x.strength > y.strength)
+                return -1;
+            else if (x.strength < y.strength)
+                return 1;
+            else
+                return 0;
+
+
+        }
+    
+
+    
+    }
+}
